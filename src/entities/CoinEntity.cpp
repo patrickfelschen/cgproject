d34fa249b91ac2288ccfcbf0c94ac28f94d181d1
patrickfelschen <patrick@felschen.de>
2022-08-26 16:23:41 +0200

    merge fix

--- src/entities/CoinEntity.cpp
+++ src/entities/CoinEntity.cpp
@@ -4,11 +4,11 @@
 #define TO_RAD(deg) (deg * std::numbers::pi / 180.0)
 #define TO_DEG(rad) (rad * 180.0 / std::numbers::pi)
 
-CoinEntity::CoinEntity(const ObjectModel *model) : Entity() {
+CoinEntity::CoinEntity(const ObjectModel* model) : Entity() {
     this->model = model;
     this->hit = false;
-    this->speed = Random::randFloat(2, 8);
-    setScaling(0.10f);
+    this->speed = Random::randFloat(1, 2);
+    setScaling(0.2f);
 }
 
 CoinEntity::~CoinEntity() = default;
@@ -16,20 +16,21 @@ CoinEntity::~CoinEntity() = default;
 void CoinEntity::respawn(const Vector3f &pos) {
     this->hit = false;
     setPosition(pos);
-    speed = Random::randFloat(2, 8);
+    speed = Random::randFloat(1, 2);
 }
 
 float yawOffset = 90;
 float pitchOffset = 20;
 void CoinEntity::update(float deltaTime) {
     Vector3f dirToTarget = (targetPosition - position).normalize();
-    setPositionVelocity(dirToTarget);
-    setRotationVelocity(Vector3f(0, 20, 0));
+
+    float pitch = asin(-dirToTarget.y);
+    float yaw = atan2(dirToTarget.x, dirToTarget.z);
 
     yaw = TO_DEG(yaw) + yawOffset;
     pitch = TO_DEG(pitch) + pitchOffset;
 
-//    Vector3f nextPos = dirToTarget * speed + Vector3f(0.0f, (gravity * deltaTime) * 3, 0.0f);
+//  Vector3f nextPos = dirToTarget speed + Vector3f(0.0f, (gravity * deltaTime) * 3, 0.0f);
 
     setRotation(Vector3f(0.0f, yaw, pitch));
     setPositionVelocity(dirToTarget * speed);
